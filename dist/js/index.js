6677ccd0d0fed9cbc6a8a5dd9acd4a83c8534163
T4kano <edutakajr@gmail.com>
2021-10-19 11:37:55 -0300

    Lose focus

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,18 +1,48 @@
-// LOADING
-$(window).on("load", function() {
-    $(".loading").fadeOut(1000);
+class MobileNavbar {
+    constructor(toggler, navList, navLinks) {
+        this.toggler = document.querySelector(toggler);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
 
-});
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-// GOOGLE MAPS
-function initMap() {
-    var location = { lat: -23.085076844458957, lng: -47.225054519829676 };
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: location
-    });
-    var marker = new google.maps.Marker({
-        position: location,
-        map: map
-    });
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                    index / 7 + 0.3
+                }s`);
+        })
+    }
+
+    handleClick() {
+        console.log(this);
+        this.navList.classList.toggle(this.activeClass);
+        this.toggler.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent() {
+        this.toggler.addEventListener("click", this.handleClick);
+    }
+
+    init() {
+        console.log("opa 1")
+        if (this.toggler) {
+            console.log("opa 2");
+            this.addClickEvent();
+        }
+        return this;
+    }
 }
+
+const mobileNavbar = new MobileNavbar(
+    ".nav__toggler",
+    ".nav__list",
+    ".nav__list li",
+)
+
+mobileNavbar.init();
